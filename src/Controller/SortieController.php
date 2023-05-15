c1e248a7983c08221ddfbb4db7023f11298a4274
Max 404 <maximekerleo@gmail.com>
2023-05-15 20:19:07 +0200

    ajout twig create pour les sortie + CSS

---
+++ src/Controller/SortieController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Entity\Sortie;
+use App\Form\SortieType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SortieController extends AbstractController
+{
+    //affichage du formulaire de création de sortie
+
+    #[Route("/sortie/create", name:"sortie_create")]
+    public function create(Request $request): Response
+    {
+        $sortie = new Sortie();
+        $sortieForm = $this->createForm(SortieType::class, $sortie);
+
+        //todo traiter le formulaire
+
+     return $this->render('create.html.twig', [
+         'sortieForm' => $sortieForm->createView()
+     ]);
+    }
+}
---
+++ src/Form/SortieType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Sortie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'label' => 'Nom de la sortie'
+            ])
+            ->add('dateHeureDebut', DateTimeType::class, [
+                'label'=> 'Date et heure de la sortie',
+              'date_widget' => 'single_text'
+            ])
+            ->add('dateLimiteInscription', DateType::class, [
+                'label'=>'Date limite d inscription',
+                'html5'=>true,
+                'widget' => 'single_text',
+            ])
+            ->add('duree',  ChoiceType::class, [
+                'label'=>'Durée',
+                'choices'=> array_combine(range(10,180), range(10, 180)),
+            ])
+            ->add('nbInscriptionsMax', ChoiceType::class, [
+                'label'=> 'Nombre de places',
+                'choices'=> array_combine(range(1, 50), range(1, 50)),
+            ])
+            ->add('infosSortie', TextareaType::class, [
+                'label'=> 'Description et infos',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}

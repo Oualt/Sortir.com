c4a2b232b5e81f872587884562359b54afbc0999
Max 404 <maximekerleo@gmail.com>
2023-05-16 09:48:57 +0200

    modif type et twig

---
+++ src/Form/CampusType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Campus;
+use App\Entity\Category;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CampusType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', EntityType::class, [
+                'class' => Campus::class,
+                'choice_label' => 'name',
+
+                'multiple' => false
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Campus::class,
+        ]);
+    }
+}

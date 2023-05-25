2f37d35c963d40503594ce7fe7fd9521aaffe571
emersonmarcantuani <emerson83@orange.fr>
2023-05-25 14:45:26 +0200

    Modif accueil, ajout des boutons RadioType. Enjoy it!

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,15 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Campus;
 use App\Entity\Sortie;
 use App\Entity\User;
-use App\Form\SearchType;
-use App\Repository\SortieRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 /**
  * @method getDoctrine()
@@ -37,30 +40,97 @@ class MainController extends AbstractController
     #[Route("/accueil", name: "main_accueil")]
     public function accueil(ManagerRegistry $doctrine, Request $request)
     {
+
+        $form = $this->createFormBuilder()
+            ->add('search_type', ChoiceType::class, [
+                'choices' => [
+                    'Sorties dont je suis l\'organisateur /trice' => 'organizer',
+                    'Sorties auxquelles je suis inscrit/e' => 'registered',
+                    'Sorties auxquelles je ne suis pas inscrit/e' => 'not_registered',
+                    'Sorties passées' => 'past',
+                ],
+                'label_attr' => [
+                    'class' => 'form-check-label',
+                ],
+                'expanded' => true,
+                'multiple' => false,
+                'placeholder' => '',
+                'required' => false,
+            ])
+            ->add('search', TextType::class, [
+                'attr' => [
+                    'class' => 'barre',
+                    'placeholder' => 'Rechercher...'
+                ],
+                'required' => false,
+            ])
+            ->add('campus', EntityType::class, [
+                'class' => Campus::class,
+                'label' => 'Campus',
+                'choice_label' => 'nom',
+                'required' => false,
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Rechercher',
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+
         $queryBuilder = $doctrine
             ->getRepository(Sortie::class)
             ->createQueryBuilder('s')
             ->leftJoin('s.etat', 'e')
             ->leftJoin('s.users', 'u');
 
-        $searchForm = $this->createForm(SearchType::class, null, [
-            'entityManager' => $this->entityManager,
-        ]);
-        $searchForm->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $searchType = $form->getData()['search_type'];
+            $search = $form->get('search')->getData();
+            $campus = $form->get('campus')->getData();
 
-        if ($searchForm->isSubmitted() && $searchForm->isValid()) {
-            $search = $searchForm->get('search')->getData();
-            $campus = $searchForm->get('campus')->getData();
             if ($campus) {
                 $queryBuilder
                     ->andWhere('s.campus = :campus')
                     ->setParameter('campus', $campus);
             }
-            $sorties = $queryBuilder
-                ->andWhere('LOWER(s.nom) LIKE :search')
-                ->setParameter('search', '%'.strtolower($search).'%')
-                ->getQuery()
-                ->getResult();
+
+            switch ($searchType) {
+                case 'organizer':
+                    $sorties = $queryBuilder
+                        ->andWhere('s.organisateur = :organisateur')
+                        ->setParameter('organisateur', $this->getUser())
+                        ->getQuery()
+                        ->getResult();
+                    break;
+                case 'registered':
+                    $sorties = $queryBuilder
+                        ->andWhere('u = :user')
+                        ->setParameter('user', $this->getUser())
+                        ->getQuery()
+                        ->getResult();
+                    break;
+                case 'not_registered':
+                    $sorties = $queryBuilder
+                        ->andWhere('u != :user')
+                        ->orWhere('u IS NULL')
+                        ->setParameter('user', $this->getUser())
+                        ->getQuery()
+                        ->getResult();
+                    break;
+                case 'past':
+                    $sorties = $queryBuilder
+                        ->andWhere('e.id = 5')
+                        ->getQuery()
+                        ->getResult();
+                    break;
+                default:
+                    $sorties = $queryBuilder
+                        ->andWhere('LOWER(s.nom) LIKE :search')
+                        ->setParameter('search', '%'.strtolower($search).'%')
+                        ->getQuery()
+                        ->getResult();
+                    break;
+            }
         } else {
             $sorties = $queryBuilder
                 ->getQuery()
@@ -68,7 +138,7 @@ class MainController extends AbstractController
         }
 
         return $this->render('accueil.html.twig', [
-            'form' => $searchForm->createView(),
+            'form' => $form->createView(),
             'sorties' => $sorties,
         ]);
     }
